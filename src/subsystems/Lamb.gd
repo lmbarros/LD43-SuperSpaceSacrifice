1b153e6c792cca5dfb8dcb1228c242cfa7ab3b27
Leandro Motta Barros <lmb@stackedboxes.org>
2018-12-02 14:13:26 -0200

    Add missing icons

---
+++ src/subsystems/Lamb.gd
@@ -0,0 +1,9 @@
+extends "res://subsystems/Subsystem.gd"
+
+var type = "item"
+var label = "Robotic Lamb"
+var description = "Looks like an old-fashioned lamb, but it is really a robotic reproduction. Sacrifice this to Borks to keep them happy for another century."
+
+
+func _init():
+	image = "res://subsystems/icon-lamb.png"
