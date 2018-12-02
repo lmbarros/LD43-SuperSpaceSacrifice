974a221168c2df5f8f7fe4b7f5218448d8c41d9b
Leandro Motta Barros <lmb@stackedboxes.org>
2018-12-02 19:48:53 -0200

    Remove and indentation level

--- src/levels/Toll.gd
+++ src/levels/Toll.gd
@@ -11,21 +11,21 @@ func _on_SacrificeWeaponAreaEntered(area):
 	# TODO: Make this random...
 
 	if ThePlayer.cargo4 && (ThePlayer.cargo4.type == "gun" || ThePlayer.cargo4.type == "bomb"):
-			ThePlayer.cargo4 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo4 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo3 && (ThePlayer.cargo3.type == "gun" || ThePlayer.cargo3.type == "bomb"):
-			ThePlayer.cargo3 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo3 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo2 && (ThePlayer.cargo2.type == "gun" || ThePlayer.cargo2.type == "bomb"):
-			ThePlayer.cargo2 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo2 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo1 && (ThePlayer.cargo1.type == "gun" || ThePlayer.cargo1.type == "bomb"):
-			ThePlayer.cargo1 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo1 = null
+		TheSound.select()
+		return
 	elif ThePlayer.gunBackward:
 		ThePlayer.gunBackward = null
 		TheSound.select()
@@ -53,21 +53,21 @@ func _onSacrificeSubsystemAreaEntered(area):
 	# TODO: Make this random...
 
 	if ThePlayer.cargo4 && (ThePlayer.cargo4.type == "engine" || ThePlayer.cargo4.type == "armor" || ThePlayer.cargo4.type == "shield"):
-			ThePlayer.cargo4 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo4 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo3 && (ThePlayer.cargo3.type == "engine" || ThePlayer.cargo3.type == "armor" || ThePlayer.cargo3.type == "shield"):
-			ThePlayer.cargo3 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo3 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo2 && (ThePlayer.cargo2.type == "engine" || ThePlayer.cargo2.type == "armor" || ThePlayer.cargo2.type == "shield"):
-			ThePlayer.cargo2 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo2 = null
+		TheSound.select()
+		return
 	elif ThePlayer.cargo1 && (ThePlayer.cargo1.type == "engine" || ThePlayer.cargo1.type == "armor" || ThePlayer.cargo1.type == "shield"):
-			ThePlayer.cargo1 = null
-			TheSound.select()
-			return
+		ThePlayer.cargo1 = null
+		TheSound.select()
+		return
 	elif ThePlayer.engine:
 		ThePlayer.engine = null
 		TheSound.select()
