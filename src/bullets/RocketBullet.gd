9ae1a135dc3f2cd2bf07333e121ef6ecc5455167
Leandro Motta Barros <lmb@stackedboxes.org>
2018-12-02 22:02:59 -0200

    Add new bullet types

---
+++ src/bullets/RocketBullet.gd
@@ -0,0 +1,38 @@
+extends "res://bullets/BaseBullet.gd"
+
+
+func _ready():
+	speed = 500
+	damage = 10
+	ignoresShields = true
+
+
+
+func init(pos, dir):
+	position = pos
+	velocity = dir * speed
+	rotation = dir.angle()
+
+
+
+func _process(delta):
+	position += velocity * delta
+	destroyIfOutOfBounds()
+
+
+
+func _isOutOfBounds():
+	return (position.x < -25 || position.x > 1300 ||
+		position.y < -25 || position.y > 800)
+
+
+
+func destroyIfOutOfBounds():
+	if _isOutOfBounds():
+		queue_free()
+
+
+
+func _onAreaEntered(area):
+	if area.collision_layer & (16 | 64) > 0: # Terrain / InstaKillers
+		queue_free()
